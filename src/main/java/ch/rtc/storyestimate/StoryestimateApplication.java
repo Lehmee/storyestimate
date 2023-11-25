b8bcd0c471cc85c2f058aecf44671515da4f280a
Lehmee <luca04.lehmann@live.de>
2023-11-25 15:09:21 +0100

    first commit

---
+++ src/main/java/ch/rtc/storyestimate/StoryestimateApplication.java
@@ -0,0 +1,13 @@
+package ch.rtc.storyestimate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoryestimateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoryestimateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/rtc/storyestimate/StoryestimateApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.rtc.storyestimate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoryestimateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
